16c0caa072c0bb54169b67bf2c3496ab3117acb1
Nicholas Romero <ncrmro@gmail.com>
2025-02-03 16:04:25 -0600

    feat: switch to enter key based saving and turn of relative line number

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Add any additional options before
+
+local opt = vim.opt
+
+-- Files
+opt.autoread = true -- automatically reload files if they change on disk
+opt.autowriteall = true -- Save files when switching buffers
+
+-- Line numbers
+opt.number = true -- always show line numbers
+opt.relativenumber = false -- disable lazyvim's relative number
