37b2cdf5ed8fb640cedb67a932c6753c416878c7
Nicholas Romero <ncrmro@gmail.com>
2026-08-26 14:49:00 -0500

    fix(hyprland): place first centered slave right

--- packages/hyprland-workstation/.config/hypr/host.lua
+++ packages/hyprland-workstation/.config/hypr/host.lua
@@ -1,2 +1,2 @@
--- ncrmro-workstation: centered master layout.
-hl.config({ master = { new_status = "slave", orientation = "center", slave_count_for_center_master = 0 } })
+-- ncrmro-workstation: centered master with the first slave on the right.
+hl.config({ master = { new_status = "slave", orientation = "center", slave_count_for_center_master = 0, center_master_fallback = "right" } })
